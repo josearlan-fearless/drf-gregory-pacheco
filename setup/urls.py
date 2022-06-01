e9f12641ffd0eef065f6840d17e5c96a4fff5e93
Jose Arlan <josearlan.fearless@gmail.com>
2022-06-01 17:15:33 -0300

    drf-gregory Criando o Endpoint de Endereços

--- setup/urls.py
+++ setup/urls.py
@@ -5,11 +5,13 @@ from rest_framework import routers
 
 from pontos_turisticos.api.viewsets import PontoTuristicoViewSet
 from atracoes.api.viewsets import AtracoesViewSet
+from enderecos.api.viewsets import EnderecoViewSet
 
 
 router = routers.DefaultRouter()
 router.register('pontosturisticos', PontoTuristicoViewSet)
 router.register('atracoes', AtracoesViewSet)
+router.register('enderecos', EnderecoViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
