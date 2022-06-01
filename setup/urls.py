78aff16182f33aa9b420b98abf1ad1f1d789d602
Jose Arlan <josearlan.fearless@gmail.com>
2022-06-01 14:59:39 -0300

    drf-gregory Criando o Endpoint de Atrações

--- setup/urls.py
+++ setup/urls.py
@@ -2,11 +2,14 @@ from django.contrib import admin
 from django.urls import path, include
 
 from rest_framework import routers
+
 from pontos_turisticos.api.viewsets import PontoTuristicoViewSet
+from atracoes.api.viewsets import AtracoesViewSet
 
 
 router = routers.DefaultRouter()
 router.register('pontosturisticos', PontoTuristicoViewSet)
+router.register('atracoes', AtracoesViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
