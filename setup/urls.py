a3a7fb5feb495f23cb69372c825cec0803776fbd
Jose Arlan <josearlan.fearless@gmail.com>
2022-06-01 19:30:22 -0300

    drf-gregory Criando o Endpoint de Comentários

--- setup/urls.py
+++ setup/urls.py
@@ -4,14 +4,16 @@ from django.urls import path, include
 from rest_framework import routers
 
 from pontos_turisticos.api.viewsets import PontoTuristicoViewSet
-from atracoes.api.viewsets import AtracoesViewSet
+from atracoes.api.viewsets import AtracaoViewSet
 from enderecos.api.viewsets import EnderecoViewSet
+from comentarios.api.viewsets import ComentarioViewSet
 
 
 router = routers.DefaultRouter()
 router.register('pontosturisticos', PontoTuristicoViewSet)
-router.register('atracoes', AtracoesViewSet)
+router.register('atracoes', AtracaoViewSet)
 router.register('enderecos', EnderecoViewSet)
+router.register('comentarios', ComentarioViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
