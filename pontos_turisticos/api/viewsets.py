2584de4b4c997299d2c8c26b24a53e3c4d7d78ae
Jose Arlan <josearlan.fearless@gmail.com>
2022-06-03 10:09:23 -0300

    drf-gregory Utilizando o SearchFilter

--- pontos_turisticos/api/viewsets.py
+++ pontos_turisticos/api/viewsets.py
@@ -1,5 +1,6 @@
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.decorators import action
+from rest_framework.filters import SearchFilter
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 
@@ -9,8 +10,9 @@ from .serializers import PontoTuristicoSerializer
 
 class PontoTuristicoViewSet(ModelViewSet):
     serializer_class = PontoTuristicoSerializer
-    filter_backends = (DjangoFilterBackend,)
+    filter_backends = (DjangoFilterBackend, SearchFilter, )
     filter_fields = ('nome', 'descricao', 'endereco')
+    search_fields = ['nome', 'descricao', 'endereco__linha1']
 
     def get_queryset(self):
         id = self.request.query_params.get('id', None)
