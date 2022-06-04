8e4593089bcfd344cd1f9640f5b06494585cb10b
Jose Arlan <josearlan.fearless@gmail.com>
2022-06-04 18:39:55 -0300

    drf-gregory ManyToMany relationships

--- pontos_turisticos/api/serializers.py
+++ pontos_turisticos/api/serializers.py
@@ -1,5 +1,7 @@
 from rest_framework.fields import SerializerMethodField
 from rest_framework.serializers import ModelSerializer
+
+from atracoes.models import Atracao
 from pontos_turisticos.models import PontoTuristico
 from atracoes.api.serializers import AtracaoSerializer
 from enderecos.api.serializers import EnderecoSerializer
@@ -8,7 +10,7 @@ from enderecos.api.serializers import EnderecoSerializer
 
 class PontoTuristicoSerializer(ModelSerializer):
     atracoes = AtracaoSerializer(many=True)
-    endereco = EnderecoSerializer()
+    endereco = EnderecoSerializer(read_only=True)
     descricao_completa = SerializerMethodField()
 
     class Meta:
@@ -16,6 +18,20 @@ class PontoTuristicoSerializer(ModelSerializer):
         fields = ['id', 'nome', 'descricao', 'aprovado', 'foto',
                   'atracoes', 'comentarios', 'avaliacoes', 'endereco',
                   'descricao_completa', 'descricao_completa2']
+        read_only_fields = ['comentarios', 'avaliacoes']
+
+    def cria_atracoes(self, atracoes, ponto):
+        for atracao in atracoes:
+            at = Atracao.objects.create(**atracao)
+            ponto.atracoes.add(at)
+
+    def create(self, validated_data):
+        # atracoes = validated_data.pop('atracoes')
+        atracoes = validated_data['atracoes']
+        del validated_data['atracoes']
+        ponto = PontoTuristico.objects.create(**validated_data)
+        self.cria_atracoes(atracoes, ponto)
+        return ponto
 
     def get_descricao_completa(self, obj):
         return f'{obj.nome} - {obj.descricao}'
