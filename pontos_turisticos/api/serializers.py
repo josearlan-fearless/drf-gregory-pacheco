d9842325d2b08a7cd2b327c739792f9898627039
Jose Arlan <josearlan.fearless@gmail.com>
2022-06-05 18:24:25 -0300

    drf-gregory Relacionando objetos com objetos existentes via ID - OneToOneField

--- pontos_turisticos/api/serializers.py
+++ pontos_turisticos/api/serializers.py
@@ -4,22 +4,30 @@ from rest_framework.serializers import ModelSerializer
 from atracoes.models import Atracao
 from enderecos.models import Endereco
 
+from pontos_turisticos.models import DocIdentificacao
 from pontos_turisticos.models import PontoTuristico
 from atracoes.api.serializers import AtracaoSerializer
 from enderecos.api.serializers import EnderecoSerializer
 # from rest_framework import serializers
 
 
+class DocIdentificacaoSerializer(ModelSerializer):
+    class Meta:
+        model = DocIdentificacao
+        fields = '__all__'
+
+
 class PontoTuristicoSerializer(ModelSerializer):
     atracoes = AtracaoSerializer(many=True)
     endereco = EnderecoSerializer()
     descricao_completa = SerializerMethodField()
+    doc_identificacao = DocIdentificacaoSerializer()
 
     class Meta:
         model = PontoTuristico
         fields = ['id', 'nome', 'descricao', 'aprovado', 'foto',
                   'atracoes', 'comentarios', 'avaliacoes', 'endereco',
-                  'descricao_completa', 'descricao_completa2']
+                  'descricao_completa', 'descricao_completa2', 'doc_identificacao']
         read_only_fields = ['comentarios', 'avaliacoes']
 
     def cria_atracoes(self, atracoes, ponto):
@@ -35,11 +43,15 @@ class PontoTuristicoSerializer(ModelSerializer):
         endereco = validated_data['endereco']
         del validated_data['endereco']
 
+        doc = validated_data.pop('doc_identificacao')
+        doci = DocIdentificacao.objects.create(**doc)
+
         ponto = PontoTuristico.objects.create(**validated_data)
         self.cria_atracoes(atracoes, ponto)
 
         end = Endereco.objects.create(**endereco)
         ponto.endereco = end
+        ponto.doc_identificacao = doci
         ponto.save()
         return ponto
 
--- pontos_turisticos/models.py
+++ pontos_turisticos/models.py
@@ -10,6 +10,13 @@ from django.dispatch import receiver
 from rest_framework.authtoken.models import Token
 
 
+class DocIdentificacao(models.Model):
+    description = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.description
+
+
 class PontoTuristico(models.Model):
     nome = models.CharField(max_length=150)
     descricao = models.TextField()
@@ -19,6 +26,8 @@ class PontoTuristico(models.Model):
     avaliacoes = models.ManyToManyField(Avaliacao)
     endereco = models.ForeignKey(Endereco, on_delete=models.CASCADE, null=True, blank=True)
     foto = models.ImageField(upload_to='pontos_turisticos', null=True, blank=True)
+    doc_identificacao = models.OneToOneField(
+        DocIdentificacao, on_delete=models.CASCADE, null=True, blank=True)
 
     def __str__(self):
         return self.nome
