04552f43e010333f26a6c770788525a3f9af823e
Jose Arlan <josearlan.fearless@gmail.com>
2022-06-06 11:42:32 -0300

    drf-gregory Relacionando objetos com objetos existentes via Action

--- pontos_turisticos/api/viewsets.py
+++ pontos_turisticos/api/viewsets.py
@@ -1,3 +1,4 @@
+from django.http import HttpResponse
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.decorators import action
@@ -34,3 +35,11 @@ class PontoTuristicoViewSet(ModelViewSet):
 
         return queryset
 
+    @action(methods=['post'], detail=True)
+    def associa_atracoes(self, request, pk):
+        atracoes = request.data['ids']
+
+        ponto = PontoTuristico.objects.get(id=pk)
+        ponto.atracoes.set(atracoes)
+        ponto.save()
+        return HttpResponse('Okay!')
